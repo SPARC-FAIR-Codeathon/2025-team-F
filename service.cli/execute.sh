29b5eadda8402540f76f18d2708379da890a7331
Omkar Athavale <oa1234@live.com>
2025-08-05 08:56:53 +1200

    function skeleton to search and retieve datasets

--- service.cli/execute.sh
+++ service.cli/execute.sh
@@ -23,8 +23,18 @@ env
 ls -al "${INPUT_FOLDER}"
 # or for example, to execute a python script on some input data:
 env
-python3 myscript.py -i path/to/input_data/input_file.txt
-cp output_file.json "${OUTPUT_FOLDER}"/outputs.json 
+
+json_input=$INPUT_FOLDER/inputs.json
+    
+INPUT_1=$(< "$json_input" jq '.input_1')
+export INPUT_1
+INPUT_2=$(< "$json_input" jq '.input_2')
+export INPUT_2
+INPUT_3=$(< "$json_input" jq '.input_3')
+export INPUT_3
+
+python3 main.py $INPUT_1 $INPUT_2 $INPUT_3
+# cp output_file.json "${OUTPUT_FOLDER}"/outputs.json 
 
 EOF
 
--- src/sparcats/main.py
+++ src/sparcats/main.py
@@ -1,1 +1,16 @@
-print("Hello")
+import search
+import util
+import json
+import sys
+
+# get url(?) to the rleevant data files
+(exact_datasets, relevant_datasets) = search.get_valid_datasets(input_form[sys.argv[1]], input_form[sys.argv[2]],input_form[sys.argv[3]])
+
+outfile_exact = "output_exact.json"
+with open('config.json', 'w') as outfile:
+    json.dump(exact_datasets, outfile)
+
+outfile_relevant = "output_relevant.json"
+with open('config.json', 'w') as outfile:
+    json.dump(relevant_datasets, outfile)
+
