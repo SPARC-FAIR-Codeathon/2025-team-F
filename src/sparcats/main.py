bc784f4d2c0f18e04253e3f3ec4c77f6c4352073
Omkar Athavale <oa1234@live.com>
2025-08-04 20:50:45 +1200

    setup script to run augmentation service

--- src/sparcats/main.py
+++ src/sparcats/main.py
@@ -1,10 +1,22 @@
 import sys
 import augment
+import warnings
+import pathlib
 
-input_data = augment.parse_json()  # Parse the inputs.json file
+input_data = augment.parse_json(sys.argv[1])  # Parse the inputs.json file
 
 nb_signals = input_data["nb_signals"]  # Number of augmented signals to create
-data = augment.read_from_csv("../../validation/input/test.csv")
+
+# Read input data
+for filepath in input_data["source_data_files"]:
+
+    if filepath.endswith('.csv'):
+        data = augment.read_from_csv(filepath)
+    elif filepath.endswith('.mat'):
+        data = augment.read_from_mat(filepath)
+    else:
+        warnings.warn(f"Unsupported file type for {filepath}. Supported types are .csv and .mat.")
+
 arr_augmenters = augment.setup_augmenter(input_data)
 augmenter = augment.create_augmenter(arr_augmenters)
 
