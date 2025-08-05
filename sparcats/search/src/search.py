e66bbe329a4f7717aad381b44122cc27bb9f0082
MMal151 <mmal151@aucklanduni.ac.nz>
2025-08-05 14:53:35 +1200

    Search utility added.

---
+++ sparcats/search/src/search.py
@@ -0,0 +1,63 @@
+from sparc_me import Dataset_Api
+from sparcats.search.src.util import get_algolia_response, traverse_list_of_dict, tags_exists
+
+dp = Dataset_Api()  # SPARC-ME object
+
+
+# Warning: May take a minute or two to run, as well of the datasets in pensieve are being checked.
+def get_valid_datasets(exp_approach="electrophysiology", species="mouse", organ="vagus nerve", sex="both"):
+    exp_approach = exp_approach.lower()
+
+    # (Future) TODO: - match lists i.e. multiple species and organs.
+    #if species is None:
+    #    animal = ["mouse"]
+    #if organ is None:
+    #    organ = ["vagus nerve"]
+
+    species = species.lower()
+    organ = organ.lower()
+    sex = sex.lower()
+
+    all_datasets = dp.get_all_datasets_latest_version_pensieve()
+    exact_dataset, relevant_datasets = "", ""
+
+    for i in all_datasets:
+        response = get_algolia_response(i["id"])
+
+        if response is not None:
+            if tags_exists(response, ["item", "modalities", "keyword"]):
+                if exp_approach in traverse_list_of_dict(response["item"]["modalities"], "keyword"):
+                    species_match, organ_match, sex_match = False, False, False
+
+                    # Double traversing as the tag contains lists of dictionaries within a list of dictionaries.
+                    # Each species have their own list.
+                    species_tag = []
+                    if tags_exists(response, ["organisms", "subject", "species", "name"]):
+                        species_tag = traverse_list_of_dict(traverse_list_of_dict(response["organisms"]["subject"],
+                                                                                  "species"), "name")
+
+                    if species in species_tag:
+                        species_match = True
+                    if (tags_exists(response, ["anatomy", "organ", "name"])
+                            and organ in traverse_list_of_dict(response["anatomy"]["organ"], "name")):
+                        organ_match = True
+                    if tags_exists(response, ["attributes", "subject", "sex", "value"]):
+                        sex_tags = traverse_list_of_dict(response["attributes"]["subject"]["sex"], "value")
+                        if sex == "both" and "male" in sex_tags and "female" in sex_tags:
+                            sex_match = True
+                        elif sex in sex_tags:
+                            sex_match = True
+
+                    if species_match and organ_match and sex_match:
+                        if exact_dataset == "":
+                            exact_dataset = str(i["id"])
+                        else:
+                            exact_dataset = exact_dataset + "," + str(i["id"])
+
+                    elif species_match or organ_match or sex_match:
+                        if relevant_datasets == "":
+                            relevant_datasets = str(i["id"])
+                        else:
+                            relevant_datasets = relevant_datasets + "," + str(i["id"])
+
+    return exact_dataset, relevant_datasets
---
+++ sparcats/search/src/util.py
@@ -0,0 +1,55 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+
+def get_algolia_response(datasetId):
+    load_dotenv(dotenv_path="../../config/.env")
+    url = f"https://04WW1V1O0F-dsn.algolia.net/1/indexes/SPARC_pr/{datasetId}"
+
+    custom_headers = {
+        'X-Algolia-API-Key': os.getenv("ALGOLIA_APP_KEY"),
+        'X-Algolia-Application-Id': os.getenv("ALGOLIA_APP_ID"),
+        'Content-Type': 'application/json',
+    }
+
+    # Make a GET request with headers
+    try:
+        response = requests.get(url, headers=custom_headers)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            #print(f"[Warning]: Request failed with status code: {response.status_code} for datasetId: {datasetId}")
+            return None
+
+    except requests.exceptions.RequestException as e:
+        print(f"[Error] An error occurred while fetching data for datasetId [{datasetId}]. "
+              f"Below are the details of the error: {e}")
+
+
+def traverse_list_of_dict(val, key="keyword"):
+    output = []
+    for i in val:
+        if key in i.keys():  # Added to ignore tags that are not required.
+            if isinstance(i[key], dict):
+                output.append(i[key])
+            else:
+                output.append(i[key].lower())
+    return output
+
+
+# List of tags to be checked in hierarchy
+def tags_exists(val, tags):
+    key = tags.pop(0)
+    if key in val.keys():
+        if len(tags) > 0:
+            if isinstance(val[key], dict):
+                return tags_exists(val[key], tags)
+            elif isinstance(val[key], list):
+                return tags_exists(val[key][0],
+                                   tags)  # Only checking one element of the list. Assumption all items under the tag will have similar tags.
+        return True
+    else:
+        return False
