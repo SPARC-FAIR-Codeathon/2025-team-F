d86f37873daf50a444f038bbf0082fd11efe6cbe
MMal151 <mmal151@aucklanduni.ac.nz>
2025-08-05 14:09:16 +1200

    New structure.

--- service.cli/execute.sh
+++ service.cli/execute.sh
@@ -14,7 +14,7 @@ echo
 # ----------------------------------------------------------------
 # This script shall be modified according to the needs in order to run the service. You can use the inputs in ${INPUT_FOLDER}
 # then retrieve the output and move it to the $OUTPUT_FOLDER as defined in the output labels
-# For example: cp output.csv $OUTPUT_FOLDER or to $OUTPUT_FOLDER/outputs.json using jq
+# For example: cp output.csv $OUTPUT_FOLDER or to $OUTPUT_FOLDER/output.json using jq
 # The inputs defined in ${INPUT_FOLDER}/inputs.json are available as env variables by their key in capital letters
 # For example: input_1 -> $INPUT_1
 
@@ -35,10 +35,10 @@ INPUT_3=$(< "$json_input" jq '.input_3')
 export INPUT_3
 
 python3 main.py $INPUT_1 $INPUT_2 $INPUT_3
-# cp output_file.json "${OUTPUT_FOLDER}"/outputs.json 
+# cp output_file.json "${OUTPUT_FOLDER}"/output.json
 
 python3 /src/main.py "validation/input_data/input_file.txt"
-#cp output_file.json "${OUTPUT_FOLDER}"/outputs.json 
+#cp output_file.json "${OUTPUT_FOLDER}"/output.json
 
 #EOF
 
